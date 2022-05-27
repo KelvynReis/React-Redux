7a6107bd65aae75c47c3d67b9317825acbbbf716
Kelvyn Reis <kelvyn2reis@gmail.com>
2022-05-27 19:45:01 -0300

    create router e page of aplication

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
 
 import { Link } from 'react-router-dom';
 import { Nav } from './styled';
 
 export default function Header() {
-  const botaoClicado = useSelector((state) => state.example.botaoClicado);
-
   return (
     <Nav>
       <Link to="/">
-        <FaHome to={24} />
+        <FaHome size={24} />
       </Link>
-      <Link to="/login">
+      <Link to="/register">
         <FaUserAlt size={24} />
       </Link>
-      <Link to="/asdad">
+      <Link to="/login">
         <FaSignInAlt size={24} />
       </Link>
-      {botaoClicado ? 'Clicado' : 'Não clicado'}
     </Nav>
   );
 }
--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,21 @@ import { Switch } from 'react-router-dom';
 import MyRoute from './MyRoute';
 
 import Login from '../pages/Login';
+import Alunos from '../pages/Alunos';
+import Aluno from '../pages/Aluno';
+import Register from '../pages/Register';
+import Fotos from '../pages/Fotos';
 import Page404 from '../pages/Page404';
 
 export default function Routers() {
   return (
     <Switch>
-      <MyRoute exact path="/" component={Login} />
+      <MyRoute exact path="/" component={Alunos} isClosed={false} />
+      <MyRoute exact path="/aluno/:id/edit" component={Aluno} isClosed />
+      <MyRoute exact path="/aluno" component={Aluno} isClosed />
+      <MyRoute exact path="/fotos/:id" component={Fotos} isClosed />
+      <MyRoute exact path="/login" component={Login} isClosed={false} />
+      <MyRoute exact path="/register" component={Register} isClosed={false} />
       <MyRoute path="*" component={Page404} />
     </Switch>
   );
